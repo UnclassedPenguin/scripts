f1328c490a6defb887e750df0785e50f7285266a
Tyler <junk@unclassed.ca>
2024-04-05 11:09:48 -0600

    update minecraft backup script

--- autominecraftbackup.sh
+++ autominecraftbackup.sh
@@ -39,23 +39,36 @@ startserver() {
 warn $"Stopping minecraft server..."
 screen -S minecraft -X stuff $' stop\n'
 sleep 30
-warn $"Done..."
 
-warn $"Checking if backup exists..."
-cd ~/backups/minecraft
-if [[ -f $date-minecraft.tar.gz ]]; then
-  warn $"Minecraft backup for $date exists already..."
-  deleteoldbackups
-  startserver 0
-else
-  warn $"Backup not found, creating..."
-  tar czf $date-minecraft.tar.gz -C ~/minecraft/worlds/ ~/minecraft/server.properties ~/minecraft/allowlist.json . || {
-    warn $"Something went wrong with minecraft backup..."
-    deleteoldbackups
-    startserver 1 
-  }
-  warn $"Backup of minecraft successful"
-  deleteoldbackups
-  startserver 0
+if [[ $(pgrep bedrock_server) ]]; then
+  warn $"Attempting to stop minecraft server again..."
+  screen -S minecraft -X stuff $' stop\n'
+  sleep 30
 fi
 
+if [[ $(pgrep bedrock_server) ]]; then
+  warn $"Server still running...exiting"
+  exit 1
+else
+  warn $"Server successfully shutdown..."
+
+  warn $"Checking if backup exists..."
+
+  cd ~/backups/minecraft
+
+  if [[ -f $date-minecraft.tar.gz ]]; then
+    warn $"Minecraft backup for $date exists already..."
+    deleteoldbackups
+    startserver 0
+  else
+    warn $"Backup not found, creating..."
+    tar czf $date-minecraft.tar.gz -C ~/minecraft/worlds/ ~/minecraft/server.properties ~/minecraft/allowlist.json . || {
+      warn $"Something went wrong with minecraft backup..."
+      deleteoldbackups
+      startserver 1 
+    }
+    warn $"Backup of minecraft successful"
+    deleteoldbackups
+    startserver 0
+  fi
+fi
